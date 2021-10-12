f7a942d45496fb8c0a08dfadc39a48872c6f8125
Connor <cs4331@rit.edu>
2021-10-12 14:58:16 -0400

    updated statuses on the grammar implementation & updated logic for param parsing

--- src/JottParser.java
+++ src/JottParser.java
@@ -16,44 +16,44 @@ public class JottParser implements JottTree {
     private static ArrayList<Token> tokens;
 
     /**
-     * program -> function_list $$                                              <-- DONE
-     * function_list -> function_def function_list                              <-- MOSTLY DONE, NEED TO ADD SUPPORT FOR MULTIPLE FUNCTIONS
+     * program -> function_list $$                                                              <-- DONE
+     * function_list -> function_def function_list                                              <-- MOSTLY DONE, NEED TO ADD SUPPORT FOR MULTIPLE FUNCTIONS
      * function_list -> ε
-     * function_def -> id [ func_def_params ] : function_return { body }        <-- DONE
-     * func_def_params -> id : type func_def_params_t|ε                         <-- DONE
-     * func_def_params_t -> , id : type func_def_params_t|ε                     <-- DONE
-     * body_stmt -> if_stmt|while_loop|stmt                                     <-- DONE
-     * return_stmt -> return expr end_stmt                                      <-- MOSTLY DONE, need to implement expr and end_stmt
-     * body -> body_stmt body|return_stmt|ε                                     <-- DONE (i think)
+     * function_def -> id [ func_def_params ] : function_return { body }                        <-- DONE
+     * func_def_params -> id : type func_def_params_t|ε                                         <-- DONE
+     * func_def_params_t -> , id : type func_def_params_t|ε                                     <-- DONE
+     * body_stmt -> if_stmt|while_loop|stmt                                                     <-- DONE
+     * return_stmt -> return expr end_stmt                                                      <-- MOSTLY DONE, need to implement expr and end_stmt
+     * body -> body_stmt body|return_stmt|ε                                                     <-- DONE (i think)
      * end_stmt -> ;
-     * if_stmt -> if [ b_expr ] { body } elseif_lst|if [ b_expr ] { body } elseif_lst else { body }
+     * if_stmt -> if [ b_expr ] { body } elseif_lst|if [ b_expr ] { body } elseif_lst else { body } <-- WORK IN PROGRESS
      * elseif_lst -> elseif [ b_expr ] { body } elseif_lst|ε
-     * while_loop -> while [ b_expr ] { body }
-     * char -> l_char|u_char|digit                                              <-- removed
-     * l_char -> a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z            <-- removed
-     * u_char -> A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z            <-- removed
-     * digit -> 0|1|2|3|4|5|6|7|8|9                                             <-- removed
-     * sign -> -|+|ε                                                            <-- removed
-     * id -> l_char char
+     * while_loop -> while [ b_expr ] { body }                                                  <--- WORK IN PROGRESS
+     * char -> l_char|u_char|digit                                                              <-- REMOVED
+     * l_char -> a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z                            <-- REMOVED
+     * u_char -> A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z                            <-- REMOVED
+     * digit -> 0|1|2|3|4|5|6|7|8|9                                                             <-- REMOVED
+     * sign -> -|+|ε                                                                            <-- REMOVED
+     * id -> l_char char                                                                        <-- DONE
      * stmt -> asmt|var_dec|func_call end_stmt
-     * func_call -> id [ params ]
-     * params -> expr params_t|ε
-     * params_t -> , expr params_t|ε
-     * expr -> i_expr|d_expr|s_expr|b_expr|id|func_call
-     * type -> Double|Integer|String|Boolean
-     * function_return -> type|Void
+     * func_call -> id [ params ]                                                               <-- WORK IN PROGRESS
+     * params -> expr params_t|ε                                                                <-- WORK IN PROGRESS
+     * params_t -> , expr params_t|ε                                                            <-- WORK IN PROGRESS
+     * expr -> i_expr|d_expr|s_expr|b_expr|id|func_call                                         <-- WORK IN PROGRESS
+     * type -> Double|Integer|String|Boolean                                                    <-- DONE
+     * function_return -> type|Void                                                             <-- DONE
      * var_dec -> type id end_stmt
      * asmt -> Double id = d_expr end_stmt|Integer id = i_expr end_stmt|String id = s_expr end_stmt|Boolean id = b_expr end_stmt|id = d_expr end_stmt|id = i_expr end_stmt|id = s_expr end_stmt|id = b_expr end_stmt
      * op -> +|*|/|+|-                                                                          <-- DONE
      * rel_op -> >|>=|<|<=|==|!=                                                                <-- DONE
-     * dbl -> sign digit . digit digit                                                          <-- removed
+     * dbl -> sign digit . digit digit                                                          <-- REMOVED
      * d_expr -> id|dbl|dbl op dbl|dbl op d_expr|d_expr op dbl|d_expr op d_expr|func_call
      * bool -> True|False                                                                       <-- DONE
-     * b_expr -> id|bool|i_expr rel_op i_expr|d_expr rel_op d_expr|s_expr rel_op s_expr|b_expr rel_op b_expr|func_call
-     * int -> sign digit digit                                                                  <-- removed
+     * b_expr -> id|bool|i_expr rel_op i_expr|d_expr rel_op d_expr|s_expr rel_op s_expr|b_expr rel_op b_expr|func_call <-- WORK IN PROGRESS
+     * int -> sign digit digit                                                                  <-- REMOVED
      * i_expr -> id|int|int op int|int op i_expr|i_expr op int|i_expr op i_expr|func_call
      * str_literal -> " str "                                                                   <-- DONE
-     * str -> char str|space str|ε                                                              <-- removed
+     * str -> char str|space str|ε                                                              <-- REMOVED
      * s_expr -> str_literal|id|func_call
      */
 
@@ -525,15 +525,31 @@ public class JottParser implements JottTree {
             return null;
         }
 
+        // find parameters
         tokenIndex += 1;
         Token paramToken = tokens.get(tokenIndex);
-        System.out.println(String.format("getToken: %s; getTokenType: %s", paramToken.getToken(), paramToken.getTokenType()));
-
+        JottTreeNode paramsNode = params(new JottTreeNode(JottElement.PARAMS));
+        if (paramsNode != null) {
+            System.out.println("found parameter(s)");
+        } else {
+            System.out.println("missing parameter(s)");
+            tokenIndex = originalTokenIndex;
+            return null;
+        }
         return null;
     }
 
     private static JottTreeNode params(JottTreeNode jottTreeNode) {
         System.out.println(JottElement.PARAMS);
+
+        Token token = tokens.get(tokenIndex);
+        JottTreeNode exprNode = expr(new JottTreeNode(JottElement.EXPR));
+        if (exprNode != null) {
+            System.out.println("an expression found!");
+        }
+
+        System.out.println(String.format("getToken: %s, getTokenType: %s", token.getToken(), token.getTokenType()));
+
         return null;
     }
 
