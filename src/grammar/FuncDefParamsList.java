ced6875f12acafad06d8e856213a3dc7e904db8d
Connor <cs4331@rit.edu>
2021-10-19 16:23:14 -0400

    stubbing classes (part 3)

--- src/grammar/FuncDefParamsList.java
+++ src/grammar/FuncDefParamsList.java
@@ -1,4 +1,23 @@
 package grammar;
 
 public class FuncDefParamsList extends FuncDefParams {
+    @Override
+    public String convertToJava() {
+        return null;
+    }
+
+    @Override
+    public String convertToC() {
+        return null;
+    }
+
+    @Override
+    public String convertToPython() {
+        return null;
+    }
+
+    @Override
+    public boolean validateTree() {
+        return false;
+    }
 }
--- src/grammar/SExpr.java
+++ src/grammar/SExpr.java
@@ -1,4 +1,5 @@
 package grammar;
 
 public class SExpr extends Expr {
+
 }
