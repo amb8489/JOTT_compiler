c52e0b898345d6b84f2fba79e67c2d47d9c4f78f
Aaron Berghash <amb8489@rit.edu>
2021-10-19 19:56:19 -0400

    type

--- src/grammar/Identifier.java
+++ src/grammar/Identifier.java
@@ -1,14 +1,19 @@
 package grammar;
 
 public class Identifier {
-    public Identifier(String token, String filename, int lineNum) {
 
+    public String id;
+    public String filename;
+    public int lineNum;
 
-
-
+    public Identifier(String id, String filename, int lineNum) {
+        this.id = id;
+        this.filename = filename;
+        this.lineNum = lineNum;
     }
 
+
     public String convertToJott() {
-        return " ";
+        return id;
     }
 }
--- src/grammar/Type.java
+++ src/grammar/Type.java
@@ -1,5 +1,10 @@
 package grammar;
 
+import main.Token;
+import main.TokenType;
+
+import java.util.ArrayList;
+
 public class Type {
     String type;
     String filename;
@@ -9,6 +14,29 @@ public class Type {
         this.type = token;
         this.filename = filename;
         this.linenum = lineNum;
+    }
+
+
+    public Type ParseType(ArrayList<Token> tokens, int nestLevel) throws ParsingException {
+        System.out.println("-------------------PARSING TYPE-----------------");
+
+        Token typeToken = tokens.remove(0);
+
+
+        if (typeToken.getTokenType() == TokenType.ID_KEYWORD) {
+            if ("Integer".equals(typeToken.getToken()) ||"Double".equals(typeToken.getToken()) ||
+                "String".equals(typeToken.getToken())  || "Boolean".equals(typeToken.getToken())) {
+                return new Type(typeToken.getToken(), typeToken.getFilename(), typeToken.getLineNum());
+            }
+        }
+        // error
+        StringBuilder sb = new StringBuilder();
+        sb.append("Syntax error\nInvalid token. Expected Integer||Double||String||Boolean. Got: ");
+        sb.append(typeToken.getTokenType().toString()).append("\n");
+        sb.append(typeToken.getFilename() + ":" +typeToken.getLineNum());
+        throw new ParsingException(sb.toString());
+
+
     }
 
     public String convertToJott(){
