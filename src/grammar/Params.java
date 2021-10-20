913477e54a3e372d267b6afb84d3ccb8dbc72cd3
Aaron Berghash <amb8489@rit.edu>
2021-10-20 13:28:25 -0400

    all of parms

---
+++ src/grammar/Params.java
@@ -0,0 +1,50 @@
+package grammar;
+
+
+import main.Token;
+
+import java.util.ArrayList;
+
+//  params -> expr param |
+//  params -> , expr params | ε
+
+public class ParamsLst {
+
+    private Expr expr;
+    ArrayList<ParamsLst> paramsLst;
+    boolean hasComma = false;
+
+    public ParamsLst(){
+
+
+    }
+
+
+    public String convertToJott() {
+        StringBuilder jstr = new StringBuilder();
+        for(ParamsLst p:paramsLst) {
+            if (p.hasComma) {
+                jstr.append(",");
+            }
+            jstr.append(expr + " ");
+        }
+        return jstr.toString();
+    }
+
+    public static ArrayList<ParamsLst> parseParams(ArrayList<Token> tokens, int nestLevel) throws ParsingException {
+        System.out.println("------------------------PARSING Params------------------------");
+
+        ArrayList<ParamsLst> list_of_params = new ArrayList<>();
+
+        // check for next token == ], this means no params
+
+        Token typeToken = tokens.remove(0);
+        System.out.println("    FIRST:" + typeToken.getToken());
+        Type type = new Type(typeToken.getToken(), typeToken.getFilename(), typeToken.getLineNum());
+        // first should have no comma or return null
+
+        // all next need , exp till , is not next token
+
+    }
+
+    }
