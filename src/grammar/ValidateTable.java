188e5dab863709437848b2d22d77e0f0ce595dc6
Aaron Berghash <amb8489@rit.edu>
2021-10-28 22:12:10 -0400

    commenting

--- src/grammar/ValidateTable.java
+++ src/grammar/ValidateTable.java
@@ -10,7 +10,9 @@ public class ValidateTable {
     public static HashMap<String, ArrayList<String>> variables = new HashMap<>();
 
 
-    // name ---> [ReturnType, params.. ]     params = tpye
+    // name ---> [ReturnType, param1,param1_type, param2,param2_type ]
+    // where param  id  = 1 + 2*n ,    where n is the idx of the params starting at 0
+    //              ids type = 2 + 2*n
 
     public static HashMap<String, ArrayList<String>> functions = new HashMap<>();
 
